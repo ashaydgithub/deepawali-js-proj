c86e4b6198c0ac0a04680fdd2390b4eefc53238b
Asha Yadav L <124244099+ashaydgithub@users.noreply.github.com>
2024-10-30 13:00:43 +0530

    Add files via upload
    happy-deepawali using html, cs, javascript

---
+++ main.js
@@ -0,0 +1,20 @@
+
+for(i=0; i<400; i++){
+  let star = document.createElementment('div');
+  star.classList.add("star");
+
+  let size = Math.random() * 2.6 + 1;
+
+  star.style.top = Math.random() * document.body.scrollHeight + "px";
+  star.style.left = Math.random() * document.body.scrollWidth + "px";
+  star.style.width = size + "px";
+  star.style.height = size + "px";
+
+
+    let delayValue = Math.random() * 4;
+
+    star.style.animationDelay = delayValue + "s"
+
+
+    document.body.appendChild(star);
+ }
